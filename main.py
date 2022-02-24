29069dd02a08684673e60dcae9574e421906af97
N3M35I5 <adwaith1998kv@gmail.com>
2022-02-24 14:17:57 +0530

    Optimization for closing

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get('https://www.google.co.in/')
+driver.quit()
